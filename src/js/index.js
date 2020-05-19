bb8fd4066092ea8a52dbcb3cec91cfc4f0661076
Asher <asher_3@hotmail.co.uk>
2020-05-19 20:54:33 +0100

    working on search functionality, having issue with recipe ID which needs addressing

--- src/js/index.js
+++ src/js/index.js
@@ -13,14 +13,15 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Get query from view
-  const query = "pizza"; //TODO
+  const query = searchView.getInput();
 
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
     // 4) Search for recipes
     await state.search.getResults();
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,31 @@ import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+  elements.searchInput.value;
+};
+
+export const clearResults = () => {
+  elements.searchResultList.innerHTML = "";
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+
+      return acc + cur.length;
+    }, 0);
+
+    return `${newTitle.join(" ")} ...)`;
+  }
+
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   const markup = `
     <li>
@@ -10,7 +35,7 @@ const renderRecipe = (recipe) => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}...</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title, 20)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
