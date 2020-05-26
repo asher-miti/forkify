a8faaac2af0dcf39a7bf2c78e54ef715f49cd9e5
Asher <asher_3@hotmail.co.uk>
2020-05-26 19:38:50 +0100

    working on updating recipe servings based on amount needed

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,14 +1,36 @@
 import { elements } from "./base";
+import { Fraction } from "fractional";
 
 export const clearRecipe = () => {
   elements.recipe.innerHTML = "";
 };
+
+const formatCount = (count) => {
+  if (count) {
+    const [int, dec] = count
+      .toString()
+      .split(".")
+      .map((el) => parseInt(el, 10));
+
+    if (!dec) return count;
+
+    if (int === 0) {
+      const fr = new Fraction(count);
+      return `${fr.numerator}/${fr.denominator}`;
+    } else {
+      const fr = new Fraction(count - int);
+      return `${int} ${fr.numerator}/${fr.denominator}`;
+    }
+  }
+  return "?";
+};
+
 const createIngredient = (ingredient) => `
     <li class="recipe__item">
     <svg class="recipe__icon">
         <use href="img/icons.svg#icon-check"></use>
     </svg>
-    <div class="recipe__count">${ingredient.count}</div>
+    <div class="recipe__count">${formatCount(ingredient.count)}</div>
     <div class="recipe__ingredient">
         <span class="recipe__unit">${ingredient.unit}</span>
         ${ingredient.ingredient}
@@ -38,7 +60,7 @@ export const renderRecipe = (recipe) => {
             <svg class="recipe__info-icon">
                 <use href="img/icons.svg#icon-man"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${recipe.info}</span>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
             <span class="recipe__info-text"> servings</span>
 
             <div class="recipe__info-buttons">
